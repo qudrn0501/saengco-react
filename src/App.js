9998f0527d815326ba12d7910bbc5d9607faaacd
qudrn0501 <104612845+qudrn0501@users.noreply.github.com>
2023-01-12 12:06:11 +0900

    update

--- src/App.js
+++ src/App.js
@@ -8,18 +8,11 @@ function Header(props) {
   console.log("props", props, props.title); //단순 props를 텍스트로 찍기 위한 콘솔로그
   return (
     <header>
-      <h1>
-        <a
-          href="/"
-          onClick={(e) => {
-            //a태그를 클릭 시 event를 다루는 함수 실행
-            e.preventDefault(); //이벤트로 발생하는 행동을 막는다.(a태그의 href 클릭으로 인해 refresh되는 것을 막음)
-            props.onChangeMode(); //앱 내 헤더 컴포넌트에서 등록한 onChangerMode prop 함수를 사용
-          }}
-        >
-          {props.title}
-        </a>
-      </h1>
+      <h1><a href="/" onClick={(e) => {
+        //a태그를 클릭 시 event를 다루는 함수 실행
+        e.preventDefault(); //이벤트로 발생하는 행동을 막는다.(a태그의 href 클릭으로 인해 refresh되는 것을 막음)
+        props.onChangeMode(); //앱 내 헤더 컴포넌트에서 등록한 onChangerMode prop 함수를 사용
+      }}>{props.title}</a></h1>
     </header>
   );
 } // App 내 헤더 컴포넌트에 적은 title 값을 {props.title} 형태로 이용, title값만 변경하면 된다.
@@ -30,24 +23,15 @@ function Nav(props) {
   const lis = [];
   for (let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];
-    lis.push(
-      <li key={t.id}>
-        <a
-          id={t.id}
-          href={"/read/" + t.id}
-          onClick={(e) => {
-            //파라미터가 1개인 경우 괄호 생략 가능
-            e.preventDefault();
-            props.onChangeMode(Number(e.target.id));
-            //e.target은 이벤트를 유발시키는 태그를 타깃으로 삼는다. .id로 하여 a태그 안의 id={t.id}를 가져온다.
-            //입력한 id는 숫자지만, a태그를 통해서 들어가는 id는 문자로 변하게 된다.
-            //Number() 내장함수를 통해서 문자열로 변환한다.
-          }}
-        >
-          {t.title}
-        </a>
-      </li>
-    );
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={"/read/" + t.id} onClick={(e) => { //파라미터가 1개인 경우 괄호 생략 가능
+        e.preventDefault();
+        props.onChangeMode(Number(e.target.id));
+        //e.target은 이벤트를 유발시키는 태그를 타깃으로 삼는다. .id로 하여 a태그 안의 id={t.id}를 가져온다.
+        //입력한 id는 숫자지만, a태그를 통해서 들어가는 id는 문자로 변하게 된다.
+        //Number() 내장함수를 통해서 문자열로 변환한다.
+      }}>{t.title}</a>
+    </li>);
   } //lis 배열을 열어 topics를 받을 준비를 한다.
   // for문을 돌려 반복 형태를 만드는데, topics 내 객체의 갯수 만큼 반복을 돌린다.
 
@@ -92,17 +76,10 @@ function Create(props) {
           const title = e.target.title.value;
           const body = e.target.body.value;
           props.onCreate(title, body);
-        }}
-      >
-        <p>
-          <input type="text" name="title" placeholder="title" />
-        </p>
-        <p>
-          <textarea name="body" placeholder="body"></textarea>
-        </p>
-        <p>
-          <input type="submit" value="Create" />
-        </p>
+        }}>
+        <p><input type="text" name="title" placeholder="title" /></p>
+        <p><textarea name="body" placeholder="body"></textarea></p>
+        <p><input type="submit" value="Create" /></p>
       </form>
     </article>
   );
@@ -115,38 +92,22 @@ function Update(props) {
   return (
     <article>
       <h2>Update</h2>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          const title = e.target.title.value;
-          const body = e.target.body.value;
-          props.onUpdate(title, body);
-        }}
-      >
-        <p>
-          <input
-            type="text"
-            name="title"
-            placeholder="title"
-            value={props.title}
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-          />
-        </p>
-        <p>
-          <textarea
-            name="body"
-            placeholder="body"
-            value={props.body}
-            onChange={(e) => {
-              setBody(e.target.value);
-            }}
-          ></textarea>
-        </p>
-        <p>
-          <input type="submit" value="Update" />
-        </p>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        const title = e.target.title.value;
+        const body = e.target.body.value;
+        props.onUpdate(title, body);
+      }}>
+        <p><input type="text" name="title" placeholder="title" value={title} 
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}/></p>
+        <p><textarea name="body" placeholder="body" value={body}
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+        ></textarea></p>
+        <p><input type="submit" value="Update" /></p>
       </form>
     </article>
   );
@@ -179,13 +140,13 @@ function App() {
     { id: 2, title: "css", body: "css is ..." },
     { id: 3, title: "js", body: "javascript is ..." },
   ]);
+
   let content = null;
   let contextControl = null;
   if (mode === "WELCOME") {
     content = <Article title="Welcome" body="Hello, Web"></Article>;
   } else if (mode === "READ") {
-    let title,
-      body = null;
+    let title, body = null;
     for (let i = 0; i < topics.length; i++) {
       if (topics[i].id === id) {
         title = topics[i].title;
@@ -194,40 +155,30 @@ function App() {
     }
     content = <Article title={title} body={body}></Article>;
     contextControl = (
-      <li>
-        <a
-          href={"/update/" + id}
-          onClick={(e) => {
-            e.preventDefault();
-            setMode("UPDATE");
-          }}
-        >
-          Update
-        </a>
-      </li>
+      <li><a href={"/update/" + id} onClick={(e) => {
+        e.preventDefault();
+        setMode("UPDATE");
+      }}>Update</a></li>
     );
   } else if (mode === "CREATE") {
     content = (
-      <Create
-        onCreate={(_title, _body) => {
-          const newTopic = { id: nextId, title: _title, body: _body };
-          //상태 만들 때 상태 데이터가 원시적인 primitive(string, number, boolean)이 아닌
-          //범 객체 데이터 object(object, array)라면
-          //데이터를 복제하고, 복제한 새로운 데이터를 변경하여 set~~함수로 값을 변경해줘야 한다.
-          //배열로 만들어진 상태 데이터(객체)는 데이터를 변경 시에 원본 데이터와 일치한지, 다른지를 비교한다.
-          //객체 데이터는 새로 들어온 데이터가 같은 데이터라면 굳이 컴포넌트를 다시 렌더링하지 않는다.
-          const newTopics = [...topics]; //topics 데이터를 newTopics로 복제
-          newTopics.push(newTopic); //복제본에 push하여 복제본을 바꾼다
-          setTopics(newTopics);
-          setMode("READ"); //글의 상세 페이지로 이동
-          setId(nextId);
-          setNextId(nextId + 1); //다음에 글을 추가할 것을 대비하여
-        }}
-      ></Create>
+      <Create onCreate={(_title, _body) => {
+        const newTopic = { id: nextId, title: _title, body: _body };
+        //상태 만들 때 상태 데이터가 원시적인 primitive(string, number, boolean)이 아닌
+        //범 객체 데이터 object(object, array)라면
+        //데이터를 복제하고, 복제한 새로운 데이터를 변경하여 set~~함수로 값을 변경해줘야 한다.
+        //배열로 만들어진 상태 데이터(객체)는 데이터를 변경 시에 원본 데이터와 일치한지, 다른지를 비교한다.
+        //객체 데이터는 새로 들어온 데이터가 같은 데이터라면 굳이 컴포넌트를 다시 렌더링하지 않는다.
+        const newTopics = [...topics]; //topics 데이터를 newTopics로 복제
+        newTopics.push(newTopic); //복제본에 push하여 복제본을 바꾼다
+        setTopics(newTopics);
+        setMode("READ"); //글의 상세 페이지로 이동
+        setId(nextId);
+        setNextId(nextId + 1); //다음에 글을 추가할 것을 대비하여
+      }}></Create>
     );
   } else if (mode === "UPDATE") {
-    let title,
-      body = null;
+    let title, body = null;
     for (let i = 0; i < topics.length; i++) {
       if (topics[i].id === id) {
         title = topics[i].title;
@@ -235,45 +186,37 @@ function App() {
       }
     }
     content = (
-      <Update
-        title={title}
-        body={body}
-        onUpdate={(title, body) => {
-          console.log(title, body);
-        }}
-      ></Update>
+      <Update title={title} body={body} onUpdate={(title, body) => {
+        const newTopics = [...topics]
+        const updatedTopic = {id:id, title:title, body:body}
+        for (let i = 0; i < newTopics.length; i++) {
+          if (newTopics[i].id === id) {
+            newTopics[i] = updatedTopic;
+            break;
+          }
+        }
+        setTopics(newTopics);
+        setMode('READ');
+      }}></Update>
     );
   }
 
   return (
     <div>
-      <Header
-        title="WEB"
-        onChangeMode={() => {
-          //onChangeMode prop을 alert 띄우는 함수로 설정
-          setMode("WELCOME");
-        }}
-      ></Header>
-      <Nav
-        topics={topics}
-        onChangeMode={(_id) => {
-          setMode("READ");
-          setId(_id);
-        }}
-      ></Nav>
+      <Header title="WEB" onChangeMode={() => {
+        //onChangeMode prop을 alert 띄우는 함수로 설정
+        setMode("WELCOME");
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(_id) => {
+        setMode("READ");
+        setId(_id);
+      }}></Nav>
       {content}
       <ul>
-        <li>
-          <a
-            href="/create"
-            onClick={(e) => {
-              e.preventDefault();
-              setMode("CREATE");
-            }}
-          >
-            Create
-          </a>
-        </li>
+        <li><a href="/create" onClick={(e) => {
+          e.preventDefault();
+          setMode("CREATE");
+        }}>Create</a></li>
         {contextControl}
       </ul>
     </div>
